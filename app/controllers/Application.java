8862d8e401bb1cb413c56be8f458ea0bebcb7a95
Kevin M. Klopfenstein <kklopfenstein@gmail.com>
2013-01-08 21:30:12 -0500

    moved bootstrap so it will work properly and got basic homepage set up

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,25 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.data.validation.Constraints.Max;
+import play.data.validation.Constraints.Min;
+import play.data.validation.Constraints.Required;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
-  
+	
+	/**
+     * Describes the hello form.
+     */
+    public static class Hello {
+        @Required public String name;
+        @Required @Min(1) @Max(100) public Integer repeat;
+        public String color;
+    } 
+    
   public static Result index() {
-    return ok(index.render("Your new application is ready."));
+    return ok(index.render("test"));
   }
   
 }
