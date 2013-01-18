0b1ee86262724dd62d952c850f588cc7cd11204d
Kevin M. Klopfenstein <kklopfenstein@gmail.com>
2013-01-17 19:09:53 -0500

    add http cache

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "org.codehaus.httpcache4j" % "httpcache4j-core" % "3.3",
+      "org.codehaus.httpcache4j.resolvers" % "resolvers-httpcomponents-httpclient" % "3.3"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
